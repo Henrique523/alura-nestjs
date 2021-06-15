76c5e93dfd43f7abab804eb48df4a66f74748b38
GUILHERME\Henrique <guilherme.hlg.eng@gmail.com>
2021-06-15 19:36:19 -0300

    feat: create interceptor for entities

--- src/usuario/usuario.entity.ts
+++ src/usuario/usuario.entity.ts
@@ -1,40 +1,31 @@
+import { Exclude, Expose } from 'class-transformer'
 import { IsEmail, IsNotEmpty, IsString } from 'class-validator'
 import { IsNomeDeUsuarioUnico } from './is-nome-de-usuario-unico.validator'
 
 export class Usuario {
   id: number
 
-  @IsNotEmpty({
-    message: 'nomeDeUsuario nao pode ser nulo.',
-  })
-  @IsString({
-    message: 'nomeDeUsuario precisa ser uma string.',
-  })
-  @IsNomeDeUsuarioUnico({
-    message: 'nomeDeUsuario precisa ser unico.',
-  })
+  @IsNotEmpty({ message: 'nomeDeUsuario nao pode ser nulo.' })
+  @IsString({ message: 'nomeDeUsuario precisa ser uma string.' })
+  @IsNomeDeUsuarioUnico({ message: 'nomeDeUsuario precisa ser unico.' })
+  @Expose({ name: 'username' })
   nomeDeUsuario: string
 
-  @IsEmail(
-    {},
-    {
-      message: 'Insira um email valido.',
-    }
-  )
-  @IsNotEmpty({
-    message: 'email nao pode ser nulo.',
-  })
+  @IsEmail({}, { message: 'Insira um email valido.' })
+  @IsNotEmpty({ message: 'email nao pode ser nulo.' })
+  @Expose({ name: 'email' })
   email: string
 
-  @IsNotEmpty({
-    message: 'senha nao pode ser nula.',
-  })
+  @IsNotEmpty({ message: 'senha nao pode ser nula.' })
+  @Exclude({ toPlainOnly: true })
+  @Expose({ name: 'password' })
   senha: string
 
-  @IsNotEmpty({
-    message: 'nomeCompleto nao pode ser nulo.',
-  })
+  @IsNotEmpty({ message: 'nomeCompleto nao pode ser nulo.' })
   @IsString()
+  @Expose({ name: 'fullName' })
   nomeCompleto: string
+
+  @Expose({ name: 'joinDate' })
   dataDeEntrada: Date
 }

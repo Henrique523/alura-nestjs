bb1dcb6d30c37a96083222841dd4df32f5577aaa
GUILHERME\Henrique <guilherme.hlg.eng@gmail.com>
2021-06-15 18:02:05 -0300

    feat: cria o modulo de usuario e suas tipagens

--- src/usuario/usuario.controller.ts
+++ src/usuario/usuario.controller.ts
@@ -1,12 +1,13 @@
 import { Body, Controller, Post } from '@nestjs/common'
+import { Usuario } from './usuario.entity'
 import { UsuarioService } from './usuario.service'
 
 @Controller('users')
 export class UsuarioController {
-  private usuarioService = new UsuarioService()
+  constructor(private usuarioService: UsuarioService) {}
 
   @Post()
-  public cria(@Body() usuario) {
+  public cria(@Body() usuario: Usuario): Usuario {
     const usuarioCriado = this.usuarioService.cria(usuario)
     return usuarioCriado
   }
